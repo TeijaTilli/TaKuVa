adcc786986b5a6614f4955ad0fe3bd629b541b88
Tuppatuuliks <roosa.turunen@gmail.com>
2021-10-29 09:03:13 +0300

    Roosan kommentti

--- app/src/main/java/com/example/takuukuittivarasto/MainActivity.kt
+++ app/src/main/java/com/example/takuukuittivarasto/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
+
+
+//Roosa Turunen
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
